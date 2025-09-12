6914e7f16a97fc2e38d8f1d4d9a258ad438a2991
EthanWong0 <ethanayaansameerkhan@gmail.com>
2025-09-12 15:58:18 +0000

    f

---
+++ Java-3-Activity/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Hi");
+    
+   System.out.println("Ethan Chase Wong");
+
+   System.out.println("Insert address here(forgot)");
+
+   System.out.println("Ethan Chase Wong\tInsert address here(forgot)");
+  }
+
+  
+}
