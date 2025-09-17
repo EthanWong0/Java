7d52399c354727d130b54a6efd3b0420c88f5a19
EthanWong0 <ethanayaansameerkhan@gmail.com>
2025-09-17 16:32:22 +0000

    f

--- Java-4-Activity/Main.java
+++ Java-4-Activity/Main.java
@@ -15,7 +15,9 @@ class Main {
    int studentAge = 16;
    int numOfPeople = 4;
    int numOfItems = 5;
-
+   System.out.println("Student age is " + studentAge);
+   System.out.println("number of people is " + numOfPeople);
+   System.out.println("Items in cart are " + numOfItems);
 
 /*  Challenge 2:
     1) temperature 
@@ -25,7 +27,9 @@ class Main {
    double temp = 78.5;
    double GPA = 3.8;
    double growthOfInv = 25.4;
-
+   System.out.println("Temperature is" + temp);
+   System.out.println("Student GPA is " + GPA);
+   System.out.println("Growth rate of investment is " + growthOfInv);
 /*  Challenge 3:
     1) Student's last name 
     2) student's first initial
@@ -34,7 +38,9 @@ class Main {
   String studentLastName = "Wong";
   char firstI = 'E';
   String ItemDesc = "Boring and useless";
-
+  System.out.println("Student last name is " + studentLastName);
+  System.out.println("Student first initial is " + firstI);
+  System.out.println("Item description is " + ItemDesc);
 /*  Challenge 4:
     1) the status whether lights are on
     2) the status whether the game is over
@@ -43,7 +49,11 @@ class Main {
   boolean lightsOn = true;
   boolean gameOver = false;
   boolean Charging = false;
-
+  
+  System.out.println("Status lights on is " + lightsOn);
+  System.out.println("Status game over is " + gameOver);
+  System.out.println("Status battery charging is " + Charging);
+  
 
     // **************************************************
     // **** Don't write any code below here.  ***********
