18bc2da2958e3e06e7305a31fd07eabed33bd107
EthanWong0 <ethanayaansameerkhan@gmail.com>
2026-01-21 17:05:56 +0000

    f

---
+++ Java-33-Activity/Main.java
@@ -0,0 +1,38 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  void print(Object o){System.out.println(o);}
+  void printt(Object o){System.out.print(o);}
+
+  void init(){
+
+	//3: create 3 new item objects
+	CartItem cartItem1 = new CartItem("Apple",1,5,true);
+  CartItem cartItem2 = new CartItem("Toothbrush",2.5,3,false);
+  CartItem cartItem3 = new CartItem("Orange",1.5,6,true);
+	
+	//4: display name & original price of items that are on sale
+	print("Items on sale: ");
+  if(cartItem1.onSale == true)
+   print(cartItem1.itemName + ": Original price is $" + cartItem1.itemPrice);
+	if(cartItem2.onSale == true)
+   print(cartItem2.itemName + ": Original price is $" + cartItem2.itemPrice);
+  if(cartItem3.onSale == true)
+      print(cartItem3.itemName + ": Original price is $" + cartItem3.itemPrice);
+	//5: subtotal (for all quantities of all items in cart, using discounted prices)
+	
+	
+	// display subtotal, tax and total
+    double subtotal = cartItem1.getPrice() + cartItem2.getPrice() + cartItem3.getPrice();
+    double tax = 0.08875;
+    double total = (subtotal*tax) + subtotal;
+
+    print("Subtotal: " + subtotal);
+    print("Tax: " + tax);
+    print("Total: " + total);
+  }
+
+}
